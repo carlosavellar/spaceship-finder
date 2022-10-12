f2f9eac735fda4952e70891d2cb2029d927fade3
carlos.avellar <carlos.avellar@gmail.com>
2022-10-12 15:43:14 -0300

    Done till the routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
 import './App.css';
+import { Login } from './Login';
+import { User } from './../model/User';
+import { AuthService } from '../service/AuthService';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { NavBar } from './NavBar';
+import { Profile } from './Profile';
+import { Home } from './Home';
 
-export class App extends React.Component {
+export interface AppState {
+  user: User | undefined;
+}
+
+export class App extends React.Component<{}, AppState> {
+  private authService = new AuthService();
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      user: undefined,
+    };
+    this.setUser = this.setUser.bind(this);
+  }
+
+  private setUser(user: User) {
+    this.setState({ user: user });
+    console.log(user);
+  }
+
   render() {
-    return <div className="App">Lav</div>;
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar user={this.state.user} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login authService={this.authService} setUser={this.setUser} />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
