643403a009bf73900e2f7e31646fbdecb0c5d2da
carlos.avellar <carlos.avellar@gmail.com>
2022-10-17 13:31:13 -0300

    beggin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,47 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Login } from './Login';
-import { User } from './../model/User';
-import { AuthService } from '../service/AuthService';
+
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { NavBar } from './NavBar';
-import { Profile } from './Profile';
-import { Home } from './Home';
 
-export interface AppState {
-  user: User | undefined;
-}
-
-export class App extends React.Component<{}, AppState> {
-  private authService = new AuthService();
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      user: undefined,
-    };
-    this.setUser = this.setUser.bind(this);
-  }
-
-  private setUser(user: User) {
-    this.setState({ user: user });
-    console.log(user);
-  }
-
+export class App extends React.Component<{}> {
   render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <div>
-            <NavBar user={this.state.user} />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login authService={this.authService} setUser={this.setUser} />} />
-              <Route path="/profile" element={<Profile authService={this.authService} user={this.state.user} />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
+    return <div className="App">eee</div>;
   }
 }
--- src/model/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  userName: string;
-  email: string;
-}
-
-export interface UserAttribute {
-  Name: string;
-  Value: string;
-}
