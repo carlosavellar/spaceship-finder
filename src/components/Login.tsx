f6d60ad597e4b176a9a03101ea34ef320b11c0e3
carlos.avellar <carlos.avellar@gmail.com>
2022-11-03 14:22:56 -0300

    Login printing

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import { AuthService } from '../services/auth';
 
 interface LoginProps {
@@ -6,27 +6,49 @@ interface LoginProps {
 }
 
 interface LoginState {
-  name: string;
+  userName: string;
   password: string;
   isLoginAttempted: boolean;
   isLoginSuccessful: boolean;
 }
 
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
 export class Login extends React.Component<LoginProps, LoginState> {
   state: LoginState = {
-    name: '',
+    userName: '',
     password: '',
     isLoginAttempted: false,
     isLoginSuccessful: false,
   };
 
+  private handlerEnteredUserName(event: CustomEvent) {
+    this.setState({ userName: event.target.value });
+  }
+
+  private handlerEnteredPassword(event: CustomEvent) {
+    this.setState({ password: event.target.value });
+  }
+
+  private async handlerSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    const result = await this.props.authService.login(this.state.userName, this.state.password);
+    if (result) {
+      console.log(result);
+    } else {
+      console.log('Wrong login');
+    }
+  }
+
   render() {
     return (
       <div>
-        <form>
-          <input value={this.state.name} type="text" />
+        <form onSubmit={(e) => this.handlerSubmit(e)}>
+          <input value={this.state.userName} type="text" onChange={(e) => this.handlerEnteredUserName(e)} />
           <br />
-          <input value={this.state.password} type="password" />
+          <input value={this.state.password} type="password" onChange={(e) => this.handlerEnteredPassword(e)} />
           <br />
           <button type="submit">Login</button>
         </form>
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,9 @@
 import { User } from '../models/user';
 export class AuthService {
-  public async login(name: string, password: string): Promise<User | undefined> {
-    if (name === 'carlos' && password === '123') {
+  public async login(userName: string, password: string): Promise<User | undefined> {
+    if (userName === 'carlos' && password === '123') {
       return {
-        name,
+        userName,
         email: 'carlos.avellar@gmail.com',
       };
     } else {
