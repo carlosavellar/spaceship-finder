7c1659c280f7b92d572a18bc7c36c09634255eeb
carlos.avellar <carlos.avellar@gmail.com>
2022-11-04 17:46:32 -0300

    User

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,28 @@ import { AuthService } from './../services/auth';
 import { Login } from './Login';
 
 interface AppState {
-  user: User;
+  user: User | undefined;
 }
 
 export class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.setUser = this.setUser.bind(this);
+  }
+
+  private setUser(user: User) {
+    if (user) {
+      this.setState({ user: user });
+    }
+    console.log(`This is the user ${user.userName} ${user.email}`);
+  }
+
   private authService: AuthService = new AuthService();
   render() {
     return (
       <div>
         <h2>Please login</h2>
-        <Login authService={this.authService} />
+        <Login authService={this.authService} setUser={this.setUser} />
       </div>
     );
   }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,10 @@
 import React, { SyntheticEvent } from 'react';
 import { AuthService } from '../services/auth';
+import { User } from '../models/user';
 
 interface LoginProps {
   authService: AuthService;
+  setUser: (user: User) => void;
 }
 
 interface LoginState {
@@ -35,14 +37,28 @@ export class Login extends React.Component<LoginProps, LoginState> {
   private async handlerSubmit(event: SyntheticEvent) {
     event.preventDefault();
     const result = await this.props.authService.login(this.state.userName, this.state.password);
+    this.setState({ isLoginAttempted: true });
+    this.setState({ userName: '' });
+    this.setState({ password: '' });
     if (result) {
       console.log(result);
+      this.setState({ isLoginSuccessful: true });
+      this.props.setUser(result);
     } else {
+      this.setState({ isLoginSuccessful: false });
       console.log('Wrong login');
     }
   }
 
   render() {
+    let isUserLoggededHTML = undefined;
+    if (this.state.isLoginAttempted) {
+      if (this.state.isLoginSuccessful) {
+        isUserLoggededHTML = <label style={{ color: 'green' }}>User logged. Welcome {this.state.userName}.</label>;
+      } else {
+        isUserLoggededHTML = <label>Loggin failed</label>;
+      }
+    }
     return (
       <div>
         <form onSubmit={(e) => this.handlerSubmit(e)}>
@@ -51,6 +67,8 @@ export class Login extends React.Component<LoginProps, LoginState> {
           <input value={this.state.password} type="password" onChange={(e) => this.handlerEnteredPassword(e)} />
           <br />
           <button type="submit">Login</button>
+          <br />
+          {isUserLoggededHTML}
         </form>
       </div>
     );
