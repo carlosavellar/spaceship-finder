42aee45b2cbae1b45fef74d1ba841d7a328353b6
carlos.avellar <carlos.avellar@gmail.com>
2022-11-17 19:32:39 -0300

    Revert "Heheh"
    This reverts commit 53523f8305d8d004a02416805247294cc06a2536.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,51 +1,8 @@
 import React from 'react';
 import './App.css';
-import { Login } from './Login';
-import { AuthService } from '../auth/auth';
-import { User } from './../models/user';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './Home';
-import { Logout } from './Logout';
-import { Navbar } from './Navbar';
-import { Spaces } from './spaces/Spaces';
-import { DataService } from '../service/dataService';
-import { Profile } from './Profile';
-interface AppState {
-  user: User | undefined;
-}
-export class App extends React.Component<{}, AppState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      user: undefined,
-    };
-    this.setUser = this.setUser.bind(this);
-  }
 
-  private setUser(user: User) {
-    this.setState({
-      user: user,
-    });
-    console.log(user);
-  }
-
-  private authService = new AuthService();
-  private dataService = new DataService();
-
+export class App extends React.Component {
   render() {
-    return (
-      <div className="wrapper">
-        <BrowserRouter>
-          <Navbar user={this.state.user || undefined} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login setUser={this.setUser} authService={this.authService} />} />
-            <Route path="/spaces" element={<Spaces dataService={this.dataService} />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/" element={<Logout />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
+    return <div className="App">Lav</div>;
   }
 }
--- src/models/spaces.ts
+++
@@ -1,6 +0,0 @@
-export interface Space {
-  spaceId: string;
-  name: string;
-  location: string;
-  photoUrl?: string;
-}
